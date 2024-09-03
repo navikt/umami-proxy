7f6ed0a86b28a26a6db4e63e3fc3b85fabfc5b0d
carl hedgren <carl.hedgren@nav.no>
2024-09-03 13:04:09 +0200

    Upgrade test service
    it's just socat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::ToSocketAddrs;
 
+use pingora::services::Service;
 use pingora_core::server::configuration::Opt;
 use pingora_core::server::Server;
 
@@ -26,7 +27,19 @@ fn main() {
 	let mut proxy_instance = pingora_proxy::http_proxy_service(
 		&amplitrude_proxy.configuration,
 		Addr {
-			// hardcode socat-echno socat -v tcp-l:1234,fork exec:'/bin/cat'
+			/* We test against this server
+			socat \
+				-v -d -d \
+				TCP-LISTEN:1234,crlf,reuseaddr,fork \
+				SYSTEM:"
+					echo HTTP/1.1 200 OK;
+					echo Content-Type\: text/plain;
+					echo;
+					echo \"Server: \$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT\";
+					echo \"Client: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\";
+				"
+
+								*/
 			addr: ("127.0.0.1", 1234)
 				.to_socket_addrs()
 				.unwrap()
@@ -36,7 +49,7 @@ fn main() {
 	);
 
 	proxy_instance.add_tcp("127.0.0.1:6191");
-
+	// amplitrude_proxy.add_service(prometheus_service_http); Pingora has a built in prometheus bit. It lives in Services somewhere
 	amplitrude_proxy.add_service(proxy_instance);
 	amplitrude_proxy.run_forever();
 }
