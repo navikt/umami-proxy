f75a94b64a78bf2b090b03bedf67ed08b44964c5
x10an14-nav <christian.chavez@nav.no>
2024-09-05 13:15:37 +0200

    Stop `--help` from potentially showing sensitive env-vals

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,19 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Config {
-	#[arg(env = "AMPLITUDE_URL", short, default_value = "eu.amplitude.com")]
-	/// Defaults to env-var of same name, api.eu.amplitude.com if not set
+	#[arg(
+		env = "AMPLITUDE_URL",
+		hide_env_values = true,
+		short,
+		default_value = "eu.amplitude.com"
+	)]
+	/// Defaults to env-var of same name, has default if not set
 	pub amplitude_addr: String,
+
 	#[arg(env = "KAFKA_INGRESS_TOPIC", short)]
 	/// Defaults to env-var of same name
 	pub kafka_ingress_topic: Option<String>,
+
 	#[arg(env = "PROJECT_KEYS_FILE", short)]
 	/// Defaults to env-var of same name
 	// TODO: Remove optional when confirmed/landed
