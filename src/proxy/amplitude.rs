ea4095c0130dd805a72129245eb32b6fa7739c15
x10an14-nav <christian.chavez@nav.no>
2024-09-05 12:18:23 +0200

    Now remove person-identifiable phone IDs

--- src/proxy/amplitude.rs
+++ src/proxy/amplitude.rs
@@ -6,10 +6,11 @@ use crate::proxy::redact::redact_json;
 
 fn clean_up_json(json: &mut Value, key: Option<&str>) {
 	match key {
-		Some(k) => redact_json(
-			json.get_mut(k)
-				.expect("Attempted to clean up non-existing key in JSON"),
-		),
+		Some(k) => {
+			if let Some(j) = json.get_mut(k) {
+				redact_json(j)
+			}
+		},
 		None => redact_json(json),
 	}
 }
@@ -35,6 +36,12 @@ pub(crate) fn process_amplitude_event(body: &Vec<u8>) -> Option<Bytes> {
 	))
 }
 
+fn remove_key(json: &mut Value, key: &str) {
+	if let Some(j) = json.get_mut(key) {
+		j.take();
+	}
+}
+
 /// This function does all the manipulation/treatment of the HTTP request body received from some browser's amplitude sdk
 /// 1. Parse JSON as some version of an Amplitude event; TODO: Check/handle multiple versions?
 ///    - NB: `amplitude::Event`'s serde implementation lossly ignores any fields
@@ -56,6 +63,12 @@ fn process_event(json: &mut Value) -> Value {
 	// Clean up anything else left
 	redact_json(json);
 
+	// Remove consumer device identificators
+	remove_key(json, "idfa");
+	remove_key(json, "idfv");
+	remove_key(json, "adid");
+	remove_key(json, "android_id");
+
 	// Obfuscate ID of event w/ID of proxy
 	event.user_id(std::env::var("NAIS_CLIENT_ID").unwrap_or(env!("CARGO_PKG_NAME").to_string()));
 
@@ -72,30 +85,7 @@ mod tests {
 	fn test_obfuscate_user_id() {
 		let mut original_json = json!({
 			"user_id": "12345",
-			"device_id": "device-98765",  // <- Should this be redacted??
 			"event_type": "button_click",
-			"event_properties": {
-				"button_name": "signup_button",
-				"color": "blue",
-				"page": "signup_page"
-			},
-			"user_properties": {
-				"age": 30,
-				"gender": "female",
-				"location": "USA"
-			},
-			"app_version": "1.0.0",
-			"platform": "iOS",
-			"os_name": "iOS",
-			"os_version": "14.4",
-			"device_brand": "Apple",
-			"device_model": "iPhone 12",
-			"time": 5,
-			"session_id": 5,
-			"insert_id": "123e4567-e89b-12d3-a456-426614174000",  // <-- This guy
-			"location_lat": 37.7749,
-			"location_lng": -122.4194,
-			"ip": "123.45.67.89"
 		});
 
 		let mut expected_json = original_json.clone();
@@ -103,6 +93,28 @@ mod tests {
 
 		// redact_json(&mut expected_json);
 
+		assert_json_include!(actual: process_event(&mut original_json), expected: expected_json);
+	}
+	#[test]
+	fn test_obfuscate_phone_ids() {
+		// Set-up
+		let mut expected_json = json!({
+			"user_id": "12345",
+			"event_type": "button_click",
+		});
+		let mut original_json = expected_json.clone();
+
+		// Test difference
+		*expected_json.get_mut("user_id").unwrap() = json!(env!("CARGO_PKG_NAME"));
+		{
+			let json = original_json.as_object_mut().unwrap();
+			assert!(None == json.insert("idfa".to_string(), json!("foo")));
+			assert!(None == json.insert("idfv".to_string(), json!("bar")));
+			assert!(None == json.insert("adid".to_string(), json!("baz")));
+			assert!(None == json.insert("android_id".to_string(), json!("baw")));
+		}
+
+		// Verify
 		assert_json_include!(actual: process_event(&mut original_json), expected: expected_json);
 	}
 }
