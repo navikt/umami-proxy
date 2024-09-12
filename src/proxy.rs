714a2940f696aeb8d99e785d6700fd986abe0a7c
carl hedgren <carl.hedgren@nav.no>
2024-09-12 16:23:50 +0200

    Move request_filter to the top as it is run before upstream_peer

--- src/proxy.rs
+++ src/proxy.rs
@@ -35,6 +35,29 @@ impl ProxyHttp for Addr {
 		}
 	}
 
+	/// Request_filter runs before anything else. We can for example set the peer here, through ctx
+	/// Block user-agent strings that match known bots
+	async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool>
+	where
+		Self::CTX: Send + Sync,
+	{
+		let user_agent = session.downstream_session.get_header("USER-AGENT").cloned();
+		match user_agent {
+			Some(ua) => match ua.to_str() {
+				Ok(ua) => {
+					let bot = isbot::Bots::default().is_bot(ua);
+					//  ^  This should be instanciated top-level
+					session.respond_error(200).await?;
+					if bot {
+						eprintln!("This request's UA matches a known bot:\n\t{ua}");
+					}
+					return Ok(bot);
+				},
+				Err(_) => return Ok(false),
+			},
+			None => Ok(false),
+		}
+	}
 	// This guy should be the amplitude host, all requests through the proxy gets sent th upstream_peer
 	async fn upstream_peer(
 		&self,
@@ -89,28 +112,5 @@ impl ProxyHttp for Addr {
 
 		upstream_request.set_uri(redact::redact_uri(&upstream_request.uri));
 		Ok(())
-	}
-
-	/// Block user-agent strings that match known bots
-	async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool>
-	where
-		Self::CTX: Send + Sync,
-	{
-		let user_agent = session.downstream_session.get_header("USER-AGENT").cloned();
-		match user_agent {
-			Some(ua) => match ua.to_str() {
-				Ok(ua) => {
-					let bot = isbot::Bots::default().is_bot(ua);
-					//  ^  This should be instanciated top-level
-					session.respond_error(200).await?;
-					if bot {
-						eprintln!("This request's UA matches a known bot:\n\t{ua}");
-					}
-					return Ok(bot);
-				},
-				Err(_) => return Ok(false),
-			},
-			None => Ok(false),
-		}
 	}
 }
